26996143c854dee3513b272b45424a508b7be37e
Nariman <tm.mamutov.nariman@gmail.com>
2023-12-28 14:35:19 +0200

    Init

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD7uvKLf2ggDdMeaeRK5kSe5Azhun2tWNs",
+  authDomain: "timewaster-eb368.firebaseapp.com",
+  projectId: "timewaster-eb368",
+  storageBucket: "timewaster-eb368.appspot.com",
+  messagingSenderId: "216196027367",
+  appId: "1:216196027367:web:1f327f71a31011c2e5a342",
+};
+
+export const app = initializeApp(firebaseConfig);
